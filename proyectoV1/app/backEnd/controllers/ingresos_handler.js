a2dcae50e2196bd56af63b681575072c29232555
FR <98113987+lucidotbat@users.noreply.github.com>
2024-05-01 20:30:15 -0600

    Creación del archivo ingresos_handler.js
    En la carpeta controllers del backend.

---
+++ proyectoV1/app/backEnd/controllers/ingresos_handler.js
@@ -0,0 +1,31 @@
+const ingresosModel = require("../models/ingresos");
+
+async function getAllIngresos() {
+    try 
+    {
+        const ingresos = await ingresosModel.find();
+        return ingresos;
+    } 
+    catch(error) 
+    {
+        console.error('Error al obtener los ingresos: ', error);
+        throw error;
+    }
+}
+
+
+async function createIngreso({ descripcion, empresa, ingresoMensual, fechaDeInicio, diaDePago }) {
+    try 
+    {
+        const nuevoIngreso = new gastosModel({ descripcion, empresa, ingresoMensual, fechaDeInicio, diaDePago });
+        await nuevoIngreso.save();
+        return nuevoIngreso;
+    } 
+    catch (error) 
+    {
+        console.error('Error al crear el gasto: ', error);
+        throw error;
+    }
+}
+
+module.exports = { getAllIngresos, createIngreso };
